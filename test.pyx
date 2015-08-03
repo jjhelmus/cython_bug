4ebd682d91a1524f84e8c0358e60e0c413e98b99
Jonathan Helmus <jjhelmus@gmail.com>
2015-08-03 18:11:22 -0500

    A better fix

--- test.pyx
+++ test.pyx
@@ -1,13 +1,11 @@
 # distutils: sources = c_funcs.c
 
 cimport _c_funcs_h
+ctypedef unsigned short unscaled
 
 cpdef scale(v, scaling_type):
-    #cdef (float (*)(_c_funcs_h.unscaled)) * SCALE_FUNCS = [
-    #        _c_funcs_h.scale_a, _c_funcs_h.scale_b]
-    cdef (float (*)(_c_funcs_h.unscaled)) SCALE_FUNCS[2]
-    SCALE_FUNCS[0] = _c_funcs_h.scale_a
-    SCALE_FUNCS[1] = _c_funcs_h.scale_b
+    cdef (float (*)(_c_funcs_h.unscaled)) * SCALE_FUNCS = [
+            _c_funcs_h.scale_a, _c_funcs_h.scale_b]
 
     return SCALE_FUNCS[scaling_type](v)
 
