42aa2aa35ea6ec37084e4e3fe7b04fbf88e8fd3e
Jonathan Helmus <jjhelmus@gmail.com>
2015-08-03 18:03:51 -0500

    Needed for Cython 0.22+

--- test.pyx
+++ test.pyx
@@ -3,8 +3,12 @@
 cimport _c_funcs_h
 
 cpdef scale(v, scaling_type):
-    cdef (float (*)(_c_funcs_h.unscaled)) * SCALE_FUNCS = [
-            _c_funcs_h.scale_a, _c_funcs_h.scale_b]
+    #cdef (float (*)(_c_funcs_h.unscaled)) * SCALE_FUNCS = [
+    #        _c_funcs_h.scale_a, _c_funcs_h.scale_b]
+    cdef (float (*)(_c_funcs_h.unscaled)) SCALE_FUNCS[2]
+    SCALE_FUNCS[0] = _c_funcs_h.scale_a
+    SCALE_FUNCS[1] = _c_funcs_h.scale_b
+
     return SCALE_FUNCS[scaling_type](v)
 
 print scale(1, 0)
